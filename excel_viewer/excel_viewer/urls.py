cb08cb6ec43a27f395c4ac32587eb77488255ca0
Ravi <r58641@gmail.com>
2019-05-09 21:35:56 +0530

    added the view for file listing + also uploading the file T/W

--- excel_viewer/excel_viewer/urls.py
+++ excel_viewer/excel_viewer/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^viewer/', include('viewer.urls', namespace='viewer')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
